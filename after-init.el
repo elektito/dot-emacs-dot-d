dc7b2710f66f0ae57901bd0f8be8e93e4600f3de
Mostafa Razavi <mostafa@sepent.com>
2014-02-09 09:59:17 +0330

    removed unnecessary code from w3m setup.

--- after-init.el
+++ after-init.el
@@ -100,10 +100,6 @@
 
 ;;;; w3m ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'w3m-load)
-(setq browse-url-browser-function 'w3m-browse-url)
-(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
-
 ;; keyboard short-cuts
 (global-set-key "\C-xm" 'browse-url-at-point)
 (global-set-key "\C-xn" 'w3m-browse-url)
